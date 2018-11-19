3d7ec243d188ff1355af506726a02e648191bfca
Stephen Rushe <srushe@users.noreply.github.com>
2018-11-19 23:07:34 +0000

    Add tests

---
+++ spec/h-entry-author-is-rel-link-to-h-card-with-url-uid_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Indieweb::Authorship do
+  let(:page) { 'h-entry-author-is-rel-link-to-h-card-with-url-uid' }
+  let(:url) { "http://author.example.com/#{page}" }
+  let(:html) { html_for(page) }
+  let(:linked_url) { URI('http://author.example.com/about-url-uid') }
+  let(:expected_data) do
+    {
+      'url' => 'http://author.example.com/about-url-uid',
+      'name' => 'Author Full Name',
+      'photo' => 'http://author.example.com/photo.jpg'
+    }
+  end
+
+  before do
+    allow(Net::HTTP).to receive(:get).with(URI(url)) { html }
+    allow(Net::HTTP).to receive(:get).with(linked_url) { html_for('about-url-uid') }
+  end
+
+  context 'when given just a URL' do
+    it { expect(described_class.identify(url)).to eq expected_data }
+  end
+
+  context 'when given both a URL and HTML' do
+    it { expect(described_class.identify(url, html)).to eq expected_data }
+  end
+end
